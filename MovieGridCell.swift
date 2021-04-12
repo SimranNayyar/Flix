309565282623c2979805b79796eec8f88ebe0986
SimNay <snayyar@ucsd.edu>
2021-04-11 23:04:44 -0700

    try

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by SIMRAN on 4/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
